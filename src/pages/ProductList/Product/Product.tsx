a9640981db02b0198917eeb41bf4cb4055319f9b
contruc1357 <congtruc1357@gmail.com>
2025-10-29 18:30:48 +0700

    Code phan trang productList

--- src/pages/ProductList/Product/Product.tsx
+++ src/pages/ProductList/Product/Product.tsx
@@ -1,6 +1,7 @@
 import { Link } from 'react-router-dom'
 import type { Product as ProductType } from '../../../types/product.type'
 import { formatCurrency, formatNumberToSocialStyle } from '../../../utils/utils'
+import ProductRating from '../../../components/ProductRating'
 
 interface Props {
   product: ProductType
@@ -30,62 +31,7 @@ export default function Product({ product }: Props) {
             </div>
           </div>
           <div className='mt-3 flex items-center justify-end'>
-            <div className='flex items-center'>
-              <div className='relative'>
-                <div className='absolute top-0 left-0 h-full overflow-hidden' style={{ width: '50%' }}>
-                  <svg viewBox='0 0 9.5 8' className='w-3 h-3 fill-current text-yellow-300'>
-                    <defs>
-                      <linearGradient id='ratingStarGradient' x1='50%' x2='50%' y1='0%' y2='100%'>
-                        <stop offset='0' stopColor='#ffca11'></stop>
-                        <stop offset='1' stopColor='#ffad27'></stop>
-                      </linearGradient>
-                      <polygon
-                        id='ratingStar'
-                        points='14.910357 6.35294118 12.4209136 7.66171903 12.896355 4.88968305 10.8823529 2.92651626 13.6656353 2.52208166 14.910357 0 16.1550787 2.52208166 18.9383611 2.92651626 16.924359 4.88968305 17.3998004 7.66171903'
-                      ></polygon>
-                    </defs>
-                    <g fill='url(#ratingStarGradient)' fillRule='evenodd' stroke='none' strokeWidth='1'>
-                      <g transform='translate(-876 -1270)'>
-                        <g transform='translate(155 992)'>
-                          <g transform='translate(600 29)'>
-                            <g transform='translate(10 239)'>
-                              <g transform='translate(101 10)'>
-                                <use stroke='#ffa727' strokeWidth='.5' xlinkHref='#ratingStar'></use>
-                              </g>
-                            </g>
-                          </g>
-                        </g>
-                      </g>
-                    </g>
-                  </svg>
-                </div>
-                <svg viewBox='0 0 9.5 8' className='w-3 h-3 fill-current text-yellow-300'>
-                  <defs>
-                    <linearGradient id='ratingStarGradient' x1='50%' x2='50%' y1='0%' y2='100%'>
-                      <stop offset='0' stopColor='#ffca11'></stop>
-                      <stop offset='1' stopColor='#ffad27'></stop>
-                    </linearGradient>
-                    <polygon
-                      id='ratingStar'
-                      points='14.910357 6.35294118 12.4209136 7.66171903 12.896355 4.88968305 10.8823529 2.92651626 13.6656353 2.52208166 14.910357 0 16.1550787 2.52208166 18.9383611 2.92651626 16.924359 4.88968305 17.3998004 7.66171903'
-                    ></polygon>
-                  </defs>
-                  <g fill='url(#ratingStarGradient)' fillRule='evenodd' stroke='none' strokeWidth='1'>
-                    <g transform='translate(-876 -1270)'>
-                      <g transform='translate(155 992)'>
-                        <g transform='translate(600 29)'>
-                          <g transform='translate(10 239)'>
-                            <g transform='translate(101 10)'>
-                              <use stroke='#ffa727' strokeWidth='.5' xlinkHref='#ratingStar'></use>
-                            </g>
-                          </g>
-                        </g>
-                      </g>
-                    </g>
-                  </g>
-                </svg>
-              </div>
-            </div>
+            <ProductRating rating={Number(product.rating)} />
             <div className='ml-2 text-sm'>
               <span>{formatNumberToSocialStyle(product.sold)}</span>
               <span className='ml-1'>Đã bán</span>
--- src/pages/ProductList/ProductList.tsx
+++ src/pages/ProductList/ProductList.tsx
@@ -1,38 +1,60 @@
 import { useQuery } from '@tanstack/react-query'
+import { omitBy, isUndefined } from 'lodash'
 import AsideFilter from './AsideFilter'
 import Product from './Product/Product'
 import SortProductList from './SortProductList'
 import useQueryParams from '../../hooks/useQueryParam'
 import productApi from '../../apis/product.api'
+import Pagination from '../../components/Pagination'
+import type { ProductListConfig } from '../../types/product.type'
+
+export type QueryConfig = {
+  [key in keyof ProductListConfig]: string
+}
 
 export default function ProductList() {
   const queryPrams = useQueryParams()
+  const queryConfig: QueryConfig = omitBy(
+    {
+      page: queryPrams.page || '1',
+      limit: queryPrams.limit,
+      sort_by: queryPrams.sort_by,
+      exclude: queryPrams.exclude,
+      name: queryPrams.name,
+      order: queryPrams.order,
+      price_max: queryPrams.price_max,
+      price_min: queryPrams.price_min,
+      rating_filter: queryPrams.rating_filter
+    },
+    isUndefined
+  )
   const { data } = useQuery({
-    queryKey: ['products', queryPrams],
+    queryKey: ['products', queryConfig],
     queryFn: () => {
-      return productApi.getProduct(queryPrams)
+      return productApi.getProduct(queryConfig as ProductListConfig)
     }
   })
-  console.log(data)
   return (
     <div className='bg-gray-200 py-6'>
       <div className='container mx-auto max-w-7xl px-4'>
-        <div className='grid grid-cols-12 gap-6'>
-          <div className='col-span-3 pl-3'>
-            <AsideFilter />
-          </div>
-          <div className='col-span-9'>
-            <SortProductList />
-            <div className='mt-6 grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 gap-3'>
-              {data &&
-                data.data.data?.products.map((product) => (
+        {data && (
+          <div className='grid grid-cols-12 gap-6'>
+            <div className='col-span-3 pl-3'>
+              <AsideFilter />
+            </div>
+            <div className='col-span-9'>
+              <SortProductList />
+              <div className='mt-6 grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 gap-3'>
+                {data.data.data?.products.map((product) => (
                   <div className='col-span-1' key={product._id}>
                     <Product product={product} />
                   </div>
                 ))}
+              </div>
+              <Pagination queryConfig={queryConfig} pageSize={data.data.data?.pagination.page_size ?? 1} />
             </div>
           </div>
-        </div>
+        )}
       </div>
     </div>
   )
